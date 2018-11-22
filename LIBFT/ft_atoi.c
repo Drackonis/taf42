39d7fe344bba911bf68394e186f391f0660f2912
Drackonis <kergastel@hotmail.fr>
2018-11-22 17:10:28 +0100

    atoi

--- LIBFT/ft_atoi.c
+++ LIBFT/ft_atoi.c
@@ -22,13 +22,13 @@ int	ft_atoi(const char *str)
 	i = 0;
 	neg = 1;
 	num = 0;
-	while (str[i] <= 32)
+	while (str[i] == '\f' || str[i] == '\t' || str[i] == '\v' ||\
+			str[i] == '\n' || str[i] == '\r' || str[i] == ' ')
 		i++;
 	if (str[i] == '-')
-	{
 		neg -= 2;
+	if (str[i] == '-' || str[i] == '+')
 		i++;
-	}
 	if ((neg || str[i - 1] == '+') && (str[i] > '9' || str[i] < '0'))
 		return (0);
 	while (str[i] <= '9' && str[i] >= '0')
@@ -42,8 +42,7 @@ int	ft_atoi(const char *str)
 **int	main(int argc, char **argv)
 **{
 **	argc++;
-**	printf("%s\n", argv[1]);
-**	printf("%d", ft_atoi(argv[1]));
-**	return (0);
+**	printf("%d\n", ft_atoi(argv[1]));
+**	return (1);
 **}
 */
